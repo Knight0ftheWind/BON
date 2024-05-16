31b13c0bb41cfa6b2d7014bd42e878a9eeff5dd8
Tyson Ashcraft <50584556+Knight0ftheWind@users.noreply.github.com>
2024-05-16 14:56:56 -0600

    Update test.R
    To test if pull to Rstudio works correctly.

--- test.R
+++ test.R
@@ -1,3 +1,6 @@
 # This is an R script that contains only comments
 
 # It is for testing purposes to determine if my github repository is connected properly
+
+# It committed and pushed properly to my github repository.
+# Now I've made these edits in github and will see if R pulls them correctly.
