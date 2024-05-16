f569ca42d2f28912c32402c3682727da36e48a58
Knight0ftheWind <77tysonashcraft@gmail.com>
2024-05-16 15:48:32 -0600

    rearrange. Update read_in

--- call_center_analysis/read_in.R
+++ call_center_analysis/read_in.R
@@ -8,3 +8,5 @@ data <- readr::read_csv("call_center_analysis/data/call_center_data.csv",
                         col_names = TRUE,
                         col_types = "ictiiiiiiciiddd") %>% 
   filter(!is.na(weekday))
+
+save(data, file = "call_center_analysis/data/the_data.RData")
